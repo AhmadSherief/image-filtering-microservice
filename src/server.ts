414e8aaad37d2fae160cdea38988ef20436ef2c0
ahmedsherif <ahmad.sherief1@gmail.com>
2023-01-27 05:03:50 +0200

    Use typescript with variables, Send 200 status code on successful image filtering request

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+import { Request, Response } from "express";
 
 (async () => {
   // Init the Express application
@@ -30,8 +31,8 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
   //! END @TODO1
 
-  app.get("/filteredimage", async (req, res) => {
-    let { image_url } = req.query;
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    let image_url: string = req.query.image_url;
 
     if (!image_url) {
       return res.status(400).send(`image_url is required`);
@@ -39,7 +40,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
     filterImageFromURL(image_url)
       .then((filteredpath) => {
-        res.sendFile(filteredpath, function (err) {
+        res.status(200).sendFile(filteredpath, function (err: Error) {
           if (err) {
             return res.status(500).send(`An error occured: ${err}`);
           } else {
@@ -54,7 +55,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}");
   });
 
